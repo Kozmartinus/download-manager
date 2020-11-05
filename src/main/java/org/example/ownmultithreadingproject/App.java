bd7da2600b0ceab2bbf8c1704d40e0f409510a52
Márton Kozma <kozmartinus@gmail.com>
2020-11-05 20:23:56 +0100

    add loading message

--- src/main/java/org/example/ownmultithreadingproject/App.java
+++ src/main/java/org/example/ownmultithreadingproject/App.java
@@ -1,19 +1,42 @@
 package org.example.ownmultithreadingproject;
 
 import java.net.http.HttpResponse;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class App {
+    public static int counter = 0;
+    private static CountDownLatch latch = new CountDownLatch(3);;
+
     public static void main(String[] args) throws InterruptedException {
 
-        long start = System.currentTimeMillis();
-        Util.getApiData("https://www.anapioficeandfire.com/api/characters");
-        Util.getApiData("https://www.breakingbadapi.com/api/characters");
-        Util.getApiData("https://www.swapi.tech/api/people");
-        long end = System.currentTimeMillis();
-        System.out.println("Time without multithreading: " + (end - start) + " ms");
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start = System.currentTimeMillis();
+                Util.getApiData("https://www.anapioficeandfire.com/api/characters");
+                Util.getApiData("https://www.breakingbadapi.com/api/characters");
+                Util.getApiData("https://www.swapi.tech/api/people");
+                long end = System.currentTimeMillis();
+                System.out.println("\rTime without multithreading: " + (end - start) + " ms");
+            }
+        });
+
+        thread.start();
+        while (!thread.getState().equals(Thread.State.TERMINATED)) {
+            switch (counter%3) {
+                case 0: System.out.print("\rLoading."); break;
+                case 1: System.out.print("\rLoading.."); break;
+                case 2: System.out.print("\rLoading..."); break;
+            }
+            counter++;
+            Thread.sleep(500);
+        }
+        counter = 0;
+
+
 
 
         long start1 = System.currentTimeMillis();
@@ -21,17 +44,31 @@ public class App {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         executorService.submit(() -> {
             Util.getApiData("https://www.anapioficeandfire.com/api/characters");
+            latch.countDown();
         });
         executorService.submit(() -> {
             Util.getApiData("https://www.breakingbadapi.com/api/characters");
+            latch.countDown();
         });
         executorService.submit(() -> {
             Util.getApiData("https://www.swapi.tech/api/people");
+            latch.countDown();
         });
 
         executorService.shutdown();
-        executorService.awaitTermination(5, TimeUnit.SECONDS);
+
+        while (latch.getCount() != 0) {
+            switch (counter%3) {
+                case 0: System.out.print("\rLoading."); break;
+                case 1: System.out.print("\rLoading.."); break;
+                case 2: System.out.print("\rLoading..."); break;
+            }
+            counter++;
+            Thread.sleep(50);
+        }
+
+//        executorService.awaitTermination(5, TimeUnit.SECONDS);
         long end1 = System.currentTimeMillis();
-        System.out.println("Time with multithreading: " + (end1 - start1) + " ms");
+        System.out.println("\rTime with multithreading: " + (end1 - start1) + " ms");
     }
 }
