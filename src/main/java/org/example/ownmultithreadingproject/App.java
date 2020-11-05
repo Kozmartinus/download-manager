ae9de9155279e3899c937f8b7dd717bb30c0985f
Márton Kozma <kozmartinus@gmail.com>
2020-11-05 19:13:25 +0100

    add api requests with and without multithreading

--- src/main/java/org/example/ownmultithreadingproject/App.java
+++ src/main/java/org/example/ownmultithreadingproject/App.java
@@ -1,7 +1,37 @@
 package org.example.ownmultithreadingproject;
 
+import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("print");
+    public static void main(String[] args) throws InterruptedException {
+
+        long start = System.currentTimeMillis();
+        Util.getApiData("https://www.anapioficeandfire.com/api/characters");
+        Util.getApiData("https://www.breakingbadapi.com/api/characters");
+        Util.getApiData("https://www.swapi.tech/api/people");
+        long end = System.currentTimeMillis();
+        System.out.println("Time without multithreading: " + (end - start) + " ms");
+
+
+        long start1 = System.currentTimeMillis();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        executorService.submit(() -> {
+            Util.getApiData("https://www.anapioficeandfire.com/api/characters");
+        });
+        executorService.submit(() -> {
+            Util.getApiData("https://www.breakingbadapi.com/api/characters");
+        });
+        executorService.submit(() -> {
+            Util.getApiData("https://www.swapi.tech/api/people");
+        });
+
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+        long end1 = System.currentTimeMillis();
+        System.out.println("Time with multithreading: " + (end1 - start1) + " ms");
     }
 }
