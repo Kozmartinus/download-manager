5a4c774f5c3acda33dcccc040adf24c02084a4b5
Márton Kozma <kozmartinus@gmail.com>
2020-11-06 19:10:18 +0100

    create pojo for files that will be downloaded

---
+++ src/main/java/org/example/ownmultithreadingproject/FileOnWeb.java
@@ -0,0 +1,19 @@
+package org.example.ownmultithreadingproject;
+
+public class FileOnWeb {
+    private String url;
+    private String filename;
+
+    public FileOnWeb(String url, String filename) {
+        this.url = url;
+        this.filename = filename;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+}
